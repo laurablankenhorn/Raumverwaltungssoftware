03134af822b6e54f2116d18d60ff547ae631c6d5
Gruenibert <maxgruen1@gmx.de>
2022-06-20 17:47:33 +0200

    Wiederaufnahme GIT.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import GUI.View;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.demo", "GUI"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+		
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(DemoApplication.class);
 
+		builder.headless(false);
+
+		ConfigurableApplicationContext context = builder.run(args);
+		
+		try {
+			View frame = context.getBean(View.class);
+			frame.setSize(850, 650);
+			frame.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/main/java/com/example/demo/RaumverwaltungsController.java
+++ src/main/java/com/example/demo/RaumverwaltungsController.java
@@ -1,6 +1,20 @@
 
 package com.example.demo;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -10,54 +24,88 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RaumverwaltungsController {
+public class RaumverwaltungsController extends JFrame {
 
-    private NamedParameterJdbcTemplate jdbcTemplateNamed;
-    private JdbcTemplate jdbcTemplate;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Autowired
+	private NamedParameterJdbcTemplate jdbcTemplateNamed;
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
-    public RaumverwaltungsController(NamedParameterJdbcTemplate jdbcTemplateNamed, JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplateNamed = jdbcTemplateNamed;
-        this.jdbcTemplate = jdbcTemplate;
-    }
+	
 
+	public RaumverwaltungsController(NamedParameterJdbcTemplate jdbcTemplateNamed, JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplateNamed = jdbcTemplateNamed;
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
-    @RequestMapping(path = "/ChangeBelegung")
-    String changeBelegung(@RequestParam("Raumnummer") String raumnummer) { //input variable
-        SqlParameterSource namedParameters = new MapSqlParameterSource().addValue("Raumnummer", raumnummer); // wie heißt Parameter?
-        String belegung = jdbcTemplateNamed.queryForObject("SELECT Zustand FROM Belegung WHERE Raumnummer = :Raumnummer", namedParameters, String.class); // hier wird etwas ausgelesen -> t und f; damit wird was auslesen können, brauchen wir die ParameterSource.. was wollen wir auslesen?
-        System.out.println(belegung); // :raumnummer wichtig! .. für mehrere Parameter ->.addvalue
-        if (belegung.equals("t")) {
-            jdbcTemplate.execute("UPDATE Belegung set Zustand = FALSE where Raumnummer = '" + raumnummer + "'");
-            return "Zustand von Raum " + raumnummer + " wurde auf belegt geaendert.";
+	@RequestMapping(path = "/ChangeBelegung")
+	public
+	String changeBelegung(@RequestParam("Raumnummer") String raumnummer) { // input variable
+		SqlParameterSource namedParameters = new MapSqlParameterSource().addValue("Raumnummer", raumnummer); // wie
+																												// heißt
+																												// Parameter?
+		String belegung = jdbcTemplateNamed.queryForObject(
+				"SELECT Zustand FROM Belegung WHERE Raumnummer = :Raumnummer", namedParameters, String.class); // hier
+																												// wird
+																												// etwas
+																												// ausgelesen
+																												// -> t
+																												// und
+																												// f;
+																												// damit
+																												// wird
+																												// was
+																												// auslesen
+																												// können,
+																												// brauchen
+																												// wir
+																												// die
+																												// ParameterSource..
+																												// was
+																												// wollen
+																												// wir
+																												// auslesen?
+		System.out.println(belegung); // :raumnummer wichtig! .. für mehrere Parameter ->.addvalue
+		if (belegung.equals("t")) {
+			jdbcTemplate.execute("UPDATE Belegung set Zustand = FALSE where Raumnummer = '" + raumnummer + "'");
+			return "Zustand von Raum " + raumnummer + " wurde auf belegt geaendert.";
 
-        } else if (belegung.equals("f")) {
-            jdbcTemplate.execute("UPDATE Belegung set Zustand = TRUE where Raumnummer = '" + raumnummer + "'");
-            return "Zustand von Raum " + raumnummer + " wurde auf frei geaendert.";
-        } else {
-            return "Hier ist etwas schief gelaufen";
-        }
+		} else if (belegung.equals("f")) {
+			jdbcTemplate.execute("UPDATE Belegung set Zustand = TRUE where Raumnummer = '" + raumnummer + "'");
+			return "Zustand von Raum " + raumnummer + " wurde auf frei geaendert.";
+		} else {
+			return "Hier ist etwas schief gelaufen";
+		}
 
-    }
+	}
 
-    @RequestMapping(path = "/ShowKapazitaet")
-    String showKapazitaet(@RequestParam("Raumnummer") String raumnummer) {
-        SqlParameterSource namedParameters = new MapSqlParameterSource().addValue("Raumnummer", raumnummer);
-        String kapazitaet = jdbcTemplateNamed.queryForObject("SELECT Sitzplaetze FROM kapazitaet WHERE Raumnummer = :Raumnummer", namedParameters, String.class);
-        System.out.println(kapazitaet);
+	@RequestMapping(path = "/ShowKapazitaet")
+	public
+	String showKapazitaet(@RequestParam("Raumnummer") String raumnummer) {
+		SqlParameterSource namedParameters = new MapSqlParameterSource().addValue("Raumnummer", raumnummer);
+		String kapazitaet = jdbcTemplateNamed.queryForObject(
+				"SELECT Sitzplaetze FROM kapazitaet WHERE Raumnummer = :Raumnummer", namedParameters, String.class);
+		System.out.println(kapazitaet);
 
-        return "Die Sitzplatzanzahl in Raum " + raumnummer + " betraegt " + kapazitaet;
-    }
+		return "Die Sitzplatzanzahl in Raum " + raumnummer + " betraegt " + kapazitaet;
+	}
 
-    @RequestMapping(path = "/ShowInventar")
-    String showInventar(@RequestParam("Raumnummer") String raumnummer) {
-        SqlParameterSource namedParameters = new MapSqlParameterSource().addValue("Raumnummer", raumnummer);
-        String inventar = jdbcTemplateNamed.queryForObject("SELECT Inhalt FROM inventar WHERE Raumnummer = :Raumnummer", namedParameters, String.class);
-        System.out.println(inventar);
+	@RequestMapping(path = "/ShowInventar")
 
-        return "Folgendes Inventar ist in Raum " + raumnummer + " vorhanden: " + inventar;
+	String showInventar(@RequestParam("Raumnummer") String raumnummer) {
+		SqlParameterSource namedParameters = new MapSqlParameterSource().addValue("Raumnummer", raumnummer);
+		String inventar = jdbcTemplateNamed.queryForObject("SELECT Inhalt FROM inventar WHERE Raumnummer = :Raumnummer",
+				namedParameters, String.class);
+		System.out.println(inventar);
 
-    }
+		return "Folgendes Inventar ist in Raum " + raumnummer + " vorhanden: " + inventar;
+
+	}
+	
+	
+
 }
-
-
-
